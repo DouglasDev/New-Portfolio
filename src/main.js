203033a9685ac6fb4beca3d839cb02c6ed70b7f1
DouglasDev <doug012310@yahoo.com>
2019-10-22 17:25:41 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VModal from 'vue-js-modal'
+import VueCarousel from 'vue-carousel';
+
+Vue.use(VueCarousel);
+Vue.use(VModal)
 
 Vue.config.productionTip = false
 
